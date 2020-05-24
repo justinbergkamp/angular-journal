d87972baaccb75f8050f4f1393019f9aac6352b5
justinbergkamp <justin.bergkamp@gmail.com>
2020-05-24 11:35:44 -0700

    Styling and word/char counters

--- dailyjournal/src/app/_components/entry/entry.component.ts
+++ dailyjournal/src/app/_components/entry/entry.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
-
 
 @Component({
   selector: 'app-entry',
@@ -11,18 +10,30 @@ import { Validators } from '@angular/forms';
 })
 export class EntryComponent implements OnInit {
 
+  day = "Monday";
+  time = "evening";
+  prompt = "What are some things you're grateful for?"
+
+  wordCount: any;
+  @ViewChild("entryText") text: ElementRef;
+  words: any = 0;
+  wordCounter() {
+    //alert(this.text.nativeElement.value)
+    this.wordCount = this.text ? this.text.nativeElement.value.split(/\s+/) : 0;
+    this.words = this.wordCount ? this.wordCount.length : 0;
+  }
+
+
   entryForm = new FormGroup({
-    date: new FormControl('', Validators.required),
-    content: new FormControl(''),
+    content: new FormControl('',Validators.required)
   });
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   onSubmit() {
-  // TODO: Use EventEmitter with form value
-  console.warn(this.entryForm.value);
-}
+    // TODO: Use EventEmitter with form value
+    console.warn(this.entryForm.value);
+  }
 
 }
